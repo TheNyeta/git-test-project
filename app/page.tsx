487debeef4775ecbffda3cddc669eeb79669e63b
Timothy Natan <timmynatan@gmail.com>
2025-05-09 11:47:53 +0700

    v.1.01
    - second test and fix permission issue on project

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly eee it works.
+            Save and see your changes instantly eee it works haleluyaaaa.
           </li>
         </ol>
 
